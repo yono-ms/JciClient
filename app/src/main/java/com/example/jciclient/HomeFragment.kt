f7895218b3b53d322e783e5e2903e8b4378f0e31
yono-ms <yono.ms@outlook.com>
2021-01-17 10:57:27 +0900

    remote list.

--- app/src/main/java/com/example/jciclient/HomeFragment.kt
+++ app/src/main/java/com/example/jciclient/HomeFragment.kt
@@ -6,7 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jciclient.database.RemoteEntity
 import com.example.jciclient.databinding.HomeFragmentBinding
+import com.example.jciclient.databinding.RemoteItemBinding
 
 class HomeFragment : BaseFragment() {
 
@@ -19,6 +25,14 @@ class HomeFragment : BaseFragment() {
         return HomeFragmentBinding.inflate(inflater).also { binding ->
             binding.viewModel = viewModel
             binding.lifecycleOwner = viewLifecycleOwner
+
+            binding.recyclerView.layoutManager = LinearLayoutManager(context)
+            binding.recyclerView.adapter = RemoteAdapter().also { adapter ->
+                viewModel.items.observe(viewLifecycleOwner) { items ->
+                    logger.info("items.changed.")
+                    adapter.submitList(items)
+                }
+            }
 
             binding.floatingActionButton.setOnClickListener {
                 findNavController().navigate(
@@ -38,5 +52,34 @@ class HomeFragment : BaseFragment() {
 
             logger.warn("TODO : ${viewModel.items}")
         }.root
+    }
+
+    class RemoteAdapter : ListAdapter<RemoteEntity, RemoteAdapter.ViewHolder>(
+        object : DiffUtil.ItemCallback<RemoteEntity>() {
+            override fun areItemsTheSame(oldItem: RemoteEntity, newItem: RemoteEntity): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: RemoteEntity, newItem: RemoteEntity): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    ) {
+        class ViewHolder(val binding: RemoteItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                RemoteItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.binding.viewModel = getItem(position)
+        }
     }
 }
--- app/src/main/java/com/example/jciclient/HomeViewModel.kt
+++ app/src/main/java/com/example/jciclient/HomeViewModel.kt
@@ -2,7 +2,7 @@ package com.example.jciclient
 
 class HomeViewModel : BaseViewModel() {
     val items by lazy {
-        logger.warn("TODO items")
-        "TODO"
+        logger.info("read items.")
+        App.db.remoteDao().getAllLiveData()
     }
 }
