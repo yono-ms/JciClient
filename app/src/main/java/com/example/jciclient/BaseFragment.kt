08c3f8963fac4175429a7140b05722ac371470da
yono-ms <yono.ms@outlook.com>
2021-02-21 21:01:35 +0900

    hide bars.

--- app/src/main/java/com/example/jciclient/BaseFragment.kt
+++ app/src/main/java/com/example/jciclient/BaseFragment.kt
@@ -1,5 +1,9 @@
 package com.example.jciclient
 
+import android.os.Build
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.fragment.app.Fragment
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -8,23 +12,30 @@ open class BaseFragment : Fragment() {
 
     val logger: Logger by lazy { LoggerFactory.getLogger(javaClass.simpleName) }
 
-//    fun hideBars() {
-//        logger.info("hideBars")
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            activity?.window?.setDecorFitsSystemWindows(false)
-//            activity?.window?.insetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.systemBars())
-//        } else {
-//            @Suppress("DEPRECATION")
-//            activity?.window?.decorView?.systemUiVisibility =
-//                View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//        }
-//    }
-//
-//    fun showBars() {
-//        logger.info("showBars")
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            activity?.window?.setDecorFitsSystemWindows(true)
-//            activity?.window?.insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.systemBars())
-//        }
-//    }
+    fun hideBars() {
+        logger.info("hideBars")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            activity?.window?.setDecorFitsSystemWindows(false)
+            activity?.window?.insetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.systemBars())
+            activity?.window?.insetsController?.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        } else {
+
+            @Suppress("DEPRECATION")
+            activity?.window?.decorView?.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+    }
+
+    fun showBars() {
+        logger.info("showBars")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            activity?.window?.setDecorFitsSystemWindows(true)
+            activity?.window?.insetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.systemBars())
+        }
+    }
 }
