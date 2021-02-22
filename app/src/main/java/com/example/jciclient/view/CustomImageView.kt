70814ca85126ca0cb2deac26862e28c9f808ff21
yono-ms <yono.ms@outlook.com>
2021-02-23 08:27:11 +0900

    drag.

--- app/src/main/java/com/example/jciclient/view/CustomImageView.kt
+++ app/src/main/java/com/example/jciclient/view/CustomImageView.kt
@@ -3,6 +3,7 @@ package com.example.jciclient.view
 import android.content.Context
 import android.graphics.Matrix
 import android.util.AttributeSet
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import androidx.appcompat.widget.AppCompatImageView
@@ -69,18 +70,70 @@ class CustomImageView @JvmOverloads constructor(
                 super.onScaleEnd(detector)
             }
 
-            fun getMatrixValue(index: Int): Float {
-                logger.info("getMatrixValue $index")
-                val values = FloatArray(9) { 0F }
-                currentMatrix.getValues(values)
-                logger.info("values=$values")
-                return values[index]
+        })
+
+    private val gestureDetector =
+        GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onScroll(
+                e1: MotionEvent?,
+                e2: MotionEvent?,
+                distanceX: Float,
+                distanceY: Float
+            ): Boolean {
+                logger.info("onScroll")
+                val imageWidth = drawable.intrinsicWidth * getMatrixValue(Matrix.MSCALE_X)
+                val imageHeight = drawable.intrinsicHeight * getMatrixValue(Matrix.MSCALE_Y)
+                val left = getMatrixValue(Matrix.MTRANS_X)
+                val right = left + imageWidth
+                val top = getMatrixValue(Matrix.MTRANS_Y)
+                val bottom = top + imageHeight
+
+                if (width >= imageWidth && height >= imageHeight) {
+                    return false
+                }
+
+                var x = -distanceX
+                var y = -distanceY
+
+                if (width > imageWidth) {
+                    x = 0F
+                } else {
+                    if (left > 0 && x > 0) {
+                        x = -left
+                    } else if (right < width && x < 0) {
+                        x = width - right
+                    }
+                }
+
+                if (height > imageHeight) {
+                    y = 0F
+                } else {
+                    if (top > 0 && y > 0) {
+                        y = top
+                    } else if (bottom < height && y < 0) {
+                        y = height - bottom
+                    }
+                }
+
+                currentMatrix.postTranslate(x, y)
+                invalidate()
+                return super.onScroll(e1, e2, distanceX, distanceY)
             }
         })
 
+    fun getMatrixValue(index: Int): Float {
+        logger.info("getMatrixValue $index")
+        val values = FloatArray(9) { 0F }
+        currentMatrix.getValues(values)
+        logger.info("values=$values")
+        return values[index]
+    }
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         imageMatrix = currentMatrix
-        return scaleGestureDetector.onTouchEvent(event)
+        gestureDetector.onTouchEvent(event)
+        scaleGestureDetector.onTouchEvent(event)
+        return true
     }
 
     init {
